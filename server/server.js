224098f569f90e640b58df4dba440416fd685d48
elinsjodin <elinsjodin@outlook.com>
2022-08-17 13:31:04 +0200

    Implemented first boilerplate for server.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ bookings: ["testingBookings"] });
+});
+
+app.listen(5000, () => {
+  console.log("Server started on 'http://localhost:5000'");
+});
