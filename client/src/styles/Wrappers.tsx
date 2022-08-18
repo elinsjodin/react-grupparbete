a382551f2ceadb284de9ef51d8d31ea256d64171
Anton <anton.sorkin98@outlook.com>
2022-08-18 12:27:54 +0200

    added use effect to recive data from backend

---
+++ client/src/styles/Wrappers.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+// wrappers => Containers => Text
+
+//wrapper for navbar
+export const NavWrapper = styled.div`
+  display: flex;
+`;
+
+//wrapper for hero section of booking page
+export const BookingHeroWrapper = styled.div`
+  display: flex;
+`;
+
+//wrapper for booking form and input
+export const AddBookingWrapper = styled.div`
+  display: flex;
+`;
