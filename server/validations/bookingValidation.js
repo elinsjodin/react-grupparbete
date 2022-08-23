020bd3a223f50b874f5d91936adb8db1360a8133
elinsjodin <elinsjodin@outlook.com>
2022-08-23 09:24:39 +0200

    Implemented changes to Joi admin validation

--- server/validations/bookingValidation.js
+++ server/validations/bookingValidation.js
@@ -1,4 +1,3 @@
-// const { Schema } = require("mongoose");
 const Joi = require("joi");
 
 const BookingValidation = {
@@ -15,12 +14,12 @@ const BookingValidation = {
   },
   editBookingValidation: {
     body: Joi.object({
-      date: Joi.date().required(),
-      time: Joi.string().required(),
-      numberOfGuests: Joi.number().required(),
-      name: Joi.string().required(),
-      email: Joi.string().required(),
-      phone: Joi.number().required(),
+      date: Joi.date(),
+      time: Joi.string(),
+      numberOfGuests: Joi.number(),
+      name: Joi.string(),
+      email: Joi.string(),
+      phone: Joi.number(),
       bookingMessage: Joi.string(),
     }),
   },
