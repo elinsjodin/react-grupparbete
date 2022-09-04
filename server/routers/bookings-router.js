a646a3585319cefafc0d2c8f0f8b8dd6085a8917
Anton <anton.sorkin98@outlook.com>
2022-09-04 20:45:34 +0200

    ad

--- server/routers/bookings-router.js
+++ server/routers/bookings-router.js
@@ -44,21 +44,3 @@ router.post(
 // });
 
 module.exports = router;
-
-// const bookings = await bookingService.GetAllBookings();
-// // await BookingService.GetAllBookings(date);
-// //get date from booking
-// const bookingbyDate = await BookingModel.find({ date: date.date });
-// console.log("date", date);
-// //filter out bookings with the matching date
-// console.log("Bookingbydate", bookingbyDate);
-// bookings.filter((date) => req.params.date === date);
-// //loop through the bookings and return the ones with the matching date
-// for (let i = 0; i < bookings.length; i++) {
-//   if (bookings[i].date === date) {
-//     console.log(bookings[i]);
-//     return bookings[i];
-//   }
-// }
-
-// GetDateBooking(req.params.date);
